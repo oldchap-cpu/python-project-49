e792617ebf729a887ce7cf5a92518f93647f3a10
oldchap-cpu <chbw.george@hotmail.com>
2024-11-05 20:34:32 +0300

    linter correct

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -5,6 +5,7 @@ import prompt
 from brain_games.games.gcf_game import gcf_game
 from brain_games.games.cli import welcome_user
 
+
 def main():
     name = welcome_user()
     print(f'Do you want a game, {name}?')
@@ -14,5 +15,7 @@ def main():
         gcf_game(name)
     else:
         print('Sorry... see you later!')
+
+
 if __name__ == '__main__':
     main()
